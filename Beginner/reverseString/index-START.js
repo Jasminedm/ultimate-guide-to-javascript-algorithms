5bf4750e5ed552b7b821ff7952b12693bb6357e9
Jasmine Moore <dajzalynn@Jasmines-MacBook-Air.local>
2022-11-13 22:27:35 -0500

    some practice up to 7

--- Beginner/reverseString/index-START.js
+++ Beginner/reverseString/index-START.js
@@ -2,13 +2,63 @@
 Given a string of text, write an algorithm that returns the text received in a reversed format. 
 E.g reverseString('algorithms') // should return 'smhtirogla'
 */
+//SITE: https://web.archive.org/web/20210610093526/https://scotch.io/courses/the-ultimate-guide-to-javascript-algorithms/string-reversal#toc-using-reduce-
+
+//SPLIT()
+// function reverseString(text) {
+// return text.split('').reverse().join('')
+// }
 
 
+//...SPREAD OPORATER 
+// function reverseString(text) {
+//     return [...text].reverse().join('')
+//     }
 
-function reverseString(text) {
-    // Code goes here
-}
+
+//FOR LOOP
+    // function reverseString(text) {
+    //     let result = ""
+    
+    //     for (let i = text.length - 1; i >= 0; i--) {
+    //         result += text[i]
+    //     }
+    
+    //     return result
+    // }
+
+//FOR OF LOOP - NEED BETTER UNDERSTANDING
+    // function reverseString(text){
+    //     let result = "";
+    
+    //     for(let char of text){
+    //         result = char + result
+    //     }
+    
+    //     return result;
+    // }
 
 
+    //RECURSIVE - ????
+    function reverseString(text) {
+        if (text === "") {
+            return ""
+        } else {
+            return reverseString(text.substr(1)) + text[0]
+        }
+    }
+
+//REDUCE()
+    function reverseString(text) {
+        return text.split("").reduce((acc, char) => char + acc, '')
+    }
+
+
+    //REDUCE & SPREAD OPPORATER
+    function reverseString(text) {
+        return [...text].reduce((acc, char) => char + acc, '')
+    }
+
+    
 
 module.exports = reverseString
